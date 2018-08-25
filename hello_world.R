549333dc4949f85518582f75afa214d2db013d52
Russ Conte <russconte@mac.com>
2018-08-25 11:20:47 -0500

    Added five new lines

--- hello_world.R
+++ hello_world.R
@@ -1,3 +1,8 @@
 print("Hello World")
 print("Here is more text")
 # she uses a version of Bash that is easily themeable
+print("Here is line one")
+print("Here is line tw0")
+print("Here is line three")
+print("Here is line four")
+print("Here is line five")
